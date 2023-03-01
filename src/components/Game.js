2f35d56fc526cd84efe633b19fa1cd133abf0567
DeyvissonEduardo <deyvissoneduardo22@gmail.com>
2023-03-01 09:44:03 -0300

    game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,29 @@
-import './Game.css';
+import "./Game.css";
 
-const Game = ({verifyLetter}) => {
-  return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>
-        Finalizar
-      </button>
-    </div>
-  )
-}
+const Game = ({ verifyLetter }) => {
+    return (
+        <div className="game">
+            <p className="points">
+                <span>Pontuação</span>
+            </p>
+            <h1>Advinhe a palavra</h1>
+            <h3 className="tip">Dica sobra a palavra</h3>
+            <div className="wordContainer">
+                <span className="letter">A</span>
+                <span className="blankSquare"></span>
+            </div>
+            <div className="letterContainer">
+                <p>Tenta acerta a letra</p>
+                <form>
+                    <input type="text" name="letter" maxLength={1} required />
+                    <button>Jogar</button>
+                </form>
+            </div>
+            <div className="wrongLettersContainer">
+                <p>Letras já utilizadas:</p>
+            </div>
+        </div>
+    );
+};
 
-export default Game
+export default Game;
